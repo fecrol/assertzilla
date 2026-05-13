71013800a92ace049b08c062adef007b143127ce
Maciej Fec <maciejfec1996@gmail.com>
2026-05-13 21:51:11 +0100

    test az-1: add test for nteractionExecutor.InteractionExecutionResult class

--- src/test/java/com/github/fecrol/assertzilla/core/InteractionExecutorTests.java
+++ src/test/java/com/github/fecrol/assertzilla/core/InteractionExecutorTests.java
@@ -1,9 +1,10 @@
-package com.github.fecrol.assertzilla;
+package com.github.fecrol.assertzilla.core;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Tags;
+import com.github.fecrol.assertzilla.core.exceptions.TestFailedException;
+import org.junit.jupiter.api.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class InteractionExecutorTests {
 
@@ -11,8 +12,35 @@ public class InteractionExecutorTests {
     @Tags({
             @Tag("unit")
     })
-    @DisplayName("")
+    @DisplayName("Interaction Execution Result Tests")
     class InteractionExecutionResultTests {
 
+        @Test
+        @Tags({
+                @Tag("TC-0001")
+        })
+        @DisplayName("TC-0001 Interaction without exception should not register a Throwable")
+        void interactionWithoutExceptionShouldNotRegisterThrowable() {
+            InteractionExecutor.InteractionExecutionResult interactionExecutionResult = InteractionExecutor
+                    .InteractionExecutionResult
+                    .forInteraction(new InteractionWithoutException());
+
+            assertThat(interactionExecutionResult.isThrowableThrown(), is(false));
+            assertThat(interactionExecutionResult.getThrownThrowable(), is(equalTo(null)));
+        }
+
+        @Test
+        @Tags({
+                @Tag("TC-0002")
+        })
+        @DisplayName("TC-0002 Interaction with exception should register a Throwable")
+        void interactionWithExceptionShouldRegisterThrowable() {
+            InteractionExecutor.InteractionExecutionResult interactionExecutionResult = InteractionExecutor
+                    .InteractionExecutionResult
+                    .forInteraction(new InteractionWithException());
+
+            assertThat(interactionExecutionResult.isThrowableThrown(), is(true));
+            assertThat(interactionExecutionResult.getThrownThrowable(), is(instanceOf(TestFailedException.class)));
+        }
     }
 }
